0d5a61a7e35c511e03c034030bebc957c6d1f56f
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-17 21:37:51 -0400

    add menu list on notification and reminder

--- src/app/pages/default-layout/app-header/reminder/reminder.component.ts
+++ src/app/pages/default-layout/app-header/reminder/reminder.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-reminder',
   templateUrl: './reminder.component.html',
   styleUrls: ['./reminder.component.css'],
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule],
+  imports: [MatButtonModule, MatMenuModule, MatIconModule],
 })
 export class ReminderComponent {
-
+    
 }
