85d61c5e03bdf9f298387c2eb74d27969bbde67d
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-17 23:04:15 -0400

    clean code on app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { ReminderComponent } from './pages/default-layout/app-header/reminder/reminder.component';
+import { NotificationComponent } from './pages/default-layout/app-header/notification/notification.component';
+import { LoanDetailComponent } from './pages/default-layout/main-body/loan-detail/loan-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultLayoutComponent } from './pages/default-layout/default-layout.component';
+import { AppSidebarComponent } from './pages/default-layout/app-sidebar/app-sidebar.component';
+import { AppHeaderComponent } from './pages/default-layout/app-header/app-header.component';
+import { DashboardComponent } from './pages/default-layout/main-body/dashboard/dashboard.component';
+import { SearchComponent } from './pages/default-layout/main-body/search/search.component';
+import { AnalystsComponent } from './pages/default-layout/main-body/analysts/analysts.component';
+import { ReportsComponent } from './pages/default-layout/main-body/reports/reports.component';
+import { AssignDialogComponent } from './dialogs/assign-dialog/assign-dialog.component';
 
 // Import Material UI modules
 import { MatDialogModule } from '@angular/material/dialog';
@@ -14,16 +27,6 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DefaultLayoutComponent } from './pages/default-layout/default-layout.component';
-import { AppSidebarComponent } from './pages/default-layout/app-sidebar/app-sidebar.component';
-import { AppHeaderComponent } from './pages/default-layout/app-header/app-header.component';
-import { DashboardComponent } from './pages/default-layout/main-body/dashboard/dashboard.component';
-import { SearchComponent } from './pages/default-layout/main-body/search/search.component';
-import { AnalystsComponent } from './pages/default-layout/main-body/analysts/analysts.component';
-import { ReportsComponent } from './pages/default-layout/main-body/reports/reports.component';
-import { AssignDialogComponent } from './dialogs/assign-dialog/assign-dialog.component';
 import { MatPaginator, MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
@@ -32,9 +35,7 @@ import { CustomPaginator } from './pages/default-layout/main-body/search/CustomP
 import { CommonModule } from '@angular/common';
 import { TableOverviewExample } from './pages/default-layout/main-body/search/test/table-overview-example/table-overview-example';
 import { MatMenuModule } from '@angular/material/menu';
-import { ReminderComponent } from './pages/default-layout/app-header/reminder/reminder.component';
-import { NotificationComponent } from './pages/default-layout/app-header/notification/notification.component';
-import { LoanDetailComponent } from './pages/default-layout/main-body/loan-detail/loan-detail.component';
+
 
 @NgModule({
   declarations: [
