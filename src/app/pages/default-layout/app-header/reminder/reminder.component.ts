a7106114b517e3707db7e4f62b325cab0bf12249
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-18 13:33:40 -0400

    update noti and reminder icons having new ones

--- src/app/pages/default-layout/app-header/reminder/reminder.component.ts
+++ src/app/pages/default-layout/app-header/reminder/reminder.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-reminder',
-  templateUrl: './reminder.component.html',
-  styleUrls: ['./reminder.component.css'],
-  standalone: true,
-  imports: [MatButtonModule, MatMenuModule, MatIconModule],
+    selector: 'app-reminder',
+    templateUrl: './reminder.component.html',
+    styleUrls: ['./reminder.component.css'],
+    standalone: true,
+    imports: [MatButtonModule, MatMenuModule, MatIconModule, CommonModule],
 })
 export class ReminderComponent {
-    constructor(private router: Router) { }
+    hasReminder: boolean = true;
+
+    constructor(private router: Router) {}
 
     navigateToOtherComponent() {
-        // Navigate to the desired component using the router.navigate() method
-        console.log("navigateToOtherComponent", this.router);
-        this.router.navigate(['/loanDetail']);
-      }
-      
+    // Navigate to the desired component using the router.navigate() method
+    this.router.navigate(['/loanDetail']);
+    }
+
+    imageClicked() {
+        this.hasReminder = false;
+    }
 }
