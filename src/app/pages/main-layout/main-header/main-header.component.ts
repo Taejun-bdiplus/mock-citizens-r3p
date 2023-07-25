3bcbb82de7306eede74ba602a29025639d8acfd5
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-25 10:43:50 -0400

    clean code

--- src/app/pages/main-layout/main-header/main-header.component.ts
+++ src/app/pages/main-layout/main-header/main-header.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { UploadDialogComponent } from '../../..//dialogs/upload-dialog/upload-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
   styleUrls: ['./main-header.component.css']
 })
-export class MainHeaderComponent {
 
+export class MainHeaderComponent {
+  constructor(private dialog: MatDialog) {}
+
+  handleButtonClick() {
+     // Check if it is the first row
+      const dialogRef = this.dialog.open(UploadDialogComponent, {
+        // Configure dialog options here
+      });
+
+ 
+      // Subscribe to dialog close event if needed
+      dialogRef.afterClosed().subscribe(result => {
+        // Handle dialog close event if needed
+      });
+    }
 }
