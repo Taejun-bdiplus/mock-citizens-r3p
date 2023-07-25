1517339df4a3773cabe88d7e5d89543ccaa2247c
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-25 08:25:42 -0400

    add report cards on Dashboard

--- src/app/pages/main-layout/main-body/dashboard/dashboard.component.ts
+++ src/app/pages/main-layout/main-body/dashboard/dashboard.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  cards: { mainHeading: string, secondHeading: string }[] = [
+    { mainHeading: '0', secondHeading: 'Pending MI FERs' },
+    { mainHeading: '0', secondHeading: 'Pending MI QAs' },
+    { mainHeading: '20', secondHeading: 'Pending FERs' },
+    { mainHeading: '30', secondHeading: 'Pending QAs' },
+    { mainHeading: '12', secondHeading: 'Pending Repurchases' },
+    { mainHeading: '28', secondHeading: 'Pending Repurchases' },
+    { mainHeading: '10', secondHeading: 'Pending Repurchases' },
+    { mainHeading: '0', secondHeading: 'Pending MI QAs' }
+  ];
 
+  showTable: boolean = false;
 }
