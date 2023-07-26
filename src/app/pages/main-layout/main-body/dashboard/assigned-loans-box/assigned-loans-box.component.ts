0cba84a32282446d4f1c708397e99010bb89ba59
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-26 19:26:03 -0400

    navigate tp analyst tab on manage button

--- src/app/pages/main-layout/main-body/dashboard/assigned-loans-box/assigned-loans-box.component.ts
+++ src/app/pages/main-layout/main-body/dashboard/assigned-loans-box/assigned-loans-box.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-assigned-loans-box',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./assigned-loans-box.component.css']
 })
 export class AssignedLoansBoxComponent {
+  constructor(private router: Router) { }
 
+  goToAnalystsComponent() {
+    // Replace 'analysts' with the actual path of your analysts component in the routing module
+    this.router.navigate(['/analysts']);
+  }
 }
