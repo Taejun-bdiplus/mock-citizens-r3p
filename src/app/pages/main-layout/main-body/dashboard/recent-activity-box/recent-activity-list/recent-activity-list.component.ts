12493788f4b6652a744903c29836376436dba1a2
Taejun Oh <taejunoh@taejuns-mbp.mynetworksettings.com>
2023-07-26 19:48:46 -0400

    add images to recent activity list

--- src/app/pages/main-layout/main-body/dashboard/recent-activity-box/recent-activity-list/recent-activity-list.component.ts
+++ src/app/pages/main-layout/main-body/dashboard/recent-activity-box/recent-activity-list/recent-activity-list.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class RecentActivityListComponent {
   listItems = [
-    { imageUrl: 'path/to/image1.jpg', firstLine: 'First Line 1', secondLine: 'Second Line 1' },
-    { imageUrl: 'path/to/image2.jpg', firstLine: 'First Line 2', secondLine: 'Second Line 2' },
-    { imageUrl: 'path/to/image3.jpg', firstLine: 'First Line 3', secondLine: 'Second Line 3' },
-    { imageUrl: 'path/to/image4.jpg', firstLine: 'First Line 4', secondLine: 'Second Line 4' },
-    { imageUrl: 'path/to/image5.jpg', firstLine: 'First Line 5', secondLine: 'Second Line 5' },
-    { imageUrl: 'path/to/image6.jpg', firstLine: 'First Line 6', secondLine: 'Second Line 6' }
+    { imageUrl: '../assets/images/user_picture.png', firstLine: 'First Line 1', secondLine: 'Second Line 1' },
+    { imageUrl: '../assets/images/user_picture.png', firstLine: 'First Line 2', secondLine: 'Second Line 2' },
+    { imageUrl: '../assets/images/user_picture.png', firstLine: 'First Line 3', secondLine: 'Second Line 3' },
+    { imageUrl: '../assets/images/user_picture.png', firstLine: 'First Line 4', secondLine: 'Second Line 4' },
+    { imageUrl: '../assets/images/user_picture.png', firstLine: 'First Line 5', secondLine: 'Second Line 5' },
+    { imageUrl: '../assets/images/user_picture.png', firstLine: 'First Line 6', secondLine: 'Second Line 6' }
   ];
 }
