707c79306b4585dfb9ead5c57e2e5735698ff681
Taejun Oh <taejunoh@Taejuns-MacBook-Pro.local>
2023-07-30 09:58:18 -0500

    formatter on main-sidebar

--- src/app/pages/main-layout/main-sidebar/main-sidebar.component.ts
+++ src/app/pages/main-layout/main-sidebar/main-sidebar.component.ts
@@ -3,33 +3,32 @@ import { Location } from '@angular/common';
 import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
-    selector: 'app-main-sidebar',
-    templateUrl: './main-sidebar.component.html',
-    styleUrls: ['./main-sidebar.component.css']
+  selector: 'app-main-sidebar',
+  templateUrl: './main-sidebar.component.html',
+  styleUrls: ['./main-sidebar.component.css'],
 })
 export class MainSidebarComponent {
-    dashboardFocused: boolean = false;
+  dashboardFocused: boolean = false;
 
-    constructor(private router: Router, private location: Location) {}
+  constructor(private router: Router, private location: Location) {}
 
-    ngOnInit() {
-        const currentUrl: string = this.location.path();
-        
-        if (currentUrl.includes('dashboard')) {
-            this.dashboardFocused = true;
-        }
+  ngOnInit() {
+    const currentUrl: string = this.location.path();
 
-        this.router.events.subscribe((event) => {
-            if (event instanceof NavigationStart) {
-                const currentUrl = event.url;
-                const isDashboardUrl = currentUrl.includes('dashboard');
-                if (isDashboardUrl) {
-                    this.dashboardFocused = true;
-                } else {
-                    this.dashboardFocused = false;
-                }
-            }
-        })
+    if (currentUrl.includes('dashboard')) {
+      this.dashboardFocused = true;
     }
 
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        const currentUrl = event.url;
+        const isDashboardUrl = currentUrl.includes('dashboard');
+        if (isDashboardUrl) {
+          this.dashboardFocused = true;
+        } else {
+          this.dashboardFocused = false;
+        }
+      }
+    });
+  }
 }
