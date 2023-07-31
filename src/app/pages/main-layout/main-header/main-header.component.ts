a4ec24a06157715c47a8e9a46a5b3afe5eed6ae2
Mohit <mohit.parashar@bdiplus>
2023-07-31 15:49:08 +0530

    done changes

--- src/app/pages/main-layout/main-header/main-header.component.ts
+++ src/app/pages/main-layout/main-header/main-header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UploadDialogComponent } from '../../..//dialogs/upload-dialog/upload-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-header',
@@ -9,7 +10,24 @@ import { MatDialog } from '@angular/material/dialog';
 })
 
 export class MainHeaderComponent {
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private http: HttpClient) {}
+
+  private apiUrl = 'https://your-api-url.com/upload';
+
+
+  upload() {
+    const data = { /* Your data to be uploaded */ };
+    this.http.post(this.apiUrl, data).subscribe(
+      response => {
+        console.log('Upload successful:', response);
+        // Handle the response or any further actions on success
+      },
+      error => {
+        console.error('Upload failed:', error);
+        // Handle the error or any further actions on failure
+      }
+    );
+  }
 
   handleButtonClick() {
      // Check if it is the first row
